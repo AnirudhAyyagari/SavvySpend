3752dd7d33df744ff07a21f9279fcdd452e5dd8d
Anirudh <ayyagari.anirudh@gmail.com>
2024-03-23 23:24:50 +1300

    Adding visualisation module

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 from welcome import display_welcome_screen
 from login import display_login_screen
 from record import record_transaction, display_transactions
+from visualisation import TransactionVisualizer
 
 
 def main():
@@ -18,15 +19,37 @@ def main():
         st.session_state["show_login"] = False
 
     if st.session_state["authenticated"]:
-        # Sidebar for pagination
+        # Sidebar for navigation
         current_page = st.sidebar.radio(
-            "Navigate", ["Record Transaction", "Recorded Transactions"]
+            "Navigate",
+            ["Record Transaction", "Recorded Transactions", "Visualizations"],
         )
 
         if current_page == "Record Transaction":
             record_transaction()
         elif current_page == "Recorded Transactions":
             display_transactions()
+        elif current_page == "Visualizations":
+            visualizer = TransactionVisualizer("transactions.csv")
+            visualizer.display_transactions()
+            visualizer.plot_transaction_amount_distribution()
+            visualizer.plot_transaction_amount_by_category()
+            visualizer.plot_transaction_amount_over_time()
+            visualizer.transaction_amount_over_time_by_category()
+            visualizer.interactive_visualizations()
+
+        # Spacer to push the logout button to the bottom
+        st.sidebar.write("")  # Empty line for spacing
+        spacer = st.sidebar.empty()  # Create an empty container for spacing
+        for _ in range(50):  # Adjust the range value to increase the space
+            spacer.write("")  # Add empty lines to the spacer container
+
+        # Logout button
+        st.sidebar.write("---")  # Horizontal line for separation
+        if st.sidebar.button("Logout"):
+            st.session_state["authenticated"] = False
+            st.experimental_rerun()  # Rerun the app to update the state
+
     elif st.session_state["show_login"]:
         # Display the login page if the 'Get Started' button was clicked
         display_login_screen()
